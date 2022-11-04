8d9d88bd270fa48b440cc968e6f489ec80008531
Mananshi <66679952+Mananshi@users.noreply.github.com>
2022-11-04 19:59:01 +0530

    Fix: Change approach

--- src/services/user.controllers.js
+++
@@ -1,8 +0,0 @@
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient();
-
-const check = async (req, res, next) => {
-    res.send({ message: 'Awesome user check ✅' })
-}
-
-module.exports = { check }
--- src/services/user.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { check } = require('./user.controllers');
-
-router.get('/', check)
-
-module.exports = router;
